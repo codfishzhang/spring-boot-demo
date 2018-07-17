2878427d710b1bea7a05a116329adb5d827dcbd0
zhangxueyu891 <codfish@mail.ustc.edu.cn>
2018-07-17 18:01:34 +0800

    初始化项目

---
+++ src/main/java/com/huawei/hrms_v2/HrmsV2Application.java
@@ -0,0 +1,12 @@
+package com.huawei.hrms_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmsV2Application.class, args);
+    }
+}
